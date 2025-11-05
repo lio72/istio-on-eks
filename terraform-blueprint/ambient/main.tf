7ad14e47064e82e86e2828a6f6bceff3587291f1
YDLJ <100246525+lio72@users.noreply.github.com>
2025-11-05 15:50:53 +0100

    add regio

--- terraform-blueprint/ambient/main.tf
+++ terraform-blueprint/ambient/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = local.region
+  region = "us-east-1"
 }
 
 provider "kubernetes" {
@@ -32,7 +32,7 @@ data "aws_availability_zones" "available" {}
 
 locals {
   name   = basename(path.cwd)
-  region = "us-west-2"
+  region = "us-east-1"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
